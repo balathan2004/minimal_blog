48ab2d71d22b71e07b7652c1495759577136d8bc
light <balathan2004@gmail.com>
2024-11-30 09:34:43 +0530

    cors

--- libs/cors.ts
+++ libs/cors.ts
@@ -1,17 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 // lib/cors.js
-//const allowedOrigins = ['http://localhost:8081', 'http://localhost:3000'];
-const allowedOrigins = ["*"]
+const allowedOrigins = ['http://localhost:8081', 'http://localhost:3000'];
+//const allowedOrigins = ["*"]
 
 export function cors(req:NextApiRequest, res:NextApiResponse) {
   const origin = req.headers.origin;
 
   if (allowedOrigins.includes("*")) {
     res.setHeader("Access-Control-Allow-Origin", "*");
-  } else if (origin && allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
+  } 
   
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
