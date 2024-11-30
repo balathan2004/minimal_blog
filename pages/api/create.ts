7ef4c4a31f9adb4b3602f68a45b5ec86e4e91cc7
light <balathan2004@gmail.com>
2024-11-30 09:11:59 +0530

    cors testing

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -8,7 +8,7 @@ import { firestore, storage } from "@/components/firebase/config";
 import { uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import fs from "fs";
 import { ref } from "@firebase/storage";
-import {doc,setDoc} from 'firebase/firestore'
+import { doc, setDoc } from "firebase/firestore";
 import { cors } from "@/libs/cors";
 export const config = {
   api: {
@@ -20,7 +20,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseConfig>
 ) {
-  if(cors(req,res)) return;
+  if (cors(req, res)) return;
   console.log("requested");
   await post(req);
   res.json({ message: "success", status: 200 });
@@ -29,31 +29,42 @@ export default async function handler(
 async function post(req: NextApiRequest) {
   const form = IncomingForm();
 
-  form.parse(req, async(err, fields, files) => {
-    console.log(fields, files);
-    const caption = fields.caption ? fields.caption[0] : "";
-    const userId= fields.userId ? fields.userId[0] : "";
-        const username= fields.username ? fields.username[0]:""
-    const file = files.file ? files.file[0] : null;
+  const postData = await new Promise<PostDataInterface|null>((resolve, reject)=>{
+    form.parse(req, async (err, fields, files) => {
 
-    const postName=`${userId}_${new Date().getTime()}`
-    const fileName=`${postName}.jpg`
-    if (file && caption && userId && username) {
-      
-      const imageUrl=await UploadImage(file, fileName);
-      const postData:PostDataInterface={
-        post_user_name:username,
-        post_name: postName,
-        post_caption: caption,
-        post_time: setDate(),
-        post_user_id: userId,
-        post_image_url: imageUrl,
+      if(err){
+        console.log(err);
+        reject(err);
       }
-      await AddDoc(postData);
+      console.log(fields, files);
+      const caption = fields.caption ? fields.caption[0] : "";
+      const userId = fields.userId ? fields.userId[0] : "";
+      const username = fields.username ? fields.username[0] : "";
+      const file = files.file ? files.file[0] : null;
 
-      console.log("doc addeed")
-    }
+      const postName = `${userId}_${new Date().getTime()}`;
+      const fileName = `${postName}.jpg`;
+      if (file && caption && userId && username) {
+        const imageUrl = await UploadImage(file, fileName);
+        const postData: PostDataInterface = {
+          post_user_name: username,
+          post_name: postName,
+          post_caption: caption,
+          post_time: setDate(),
+          post_user_id: userId,
+          post_image_url: imageUrl,
+        };
+        console.log(postData);
+        resolve(postData);
+      }
+      resolve(null)
+    });
   });
+  if(postData){
+   
+    await AddDoc(postData);
+  }
+ 
 }
 
 async function UploadImage(file: formidable.File, fileName: string) {
@@ -72,12 +83,10 @@ async function UploadImage(file: formidable.File, fileName: string) {
   }
 }
 
-async function AddDoc(postData:PostDataInterface){
-  const postRef = doc(firestore,"posts",postData.post_name);
+async function AddDoc(postData: PostDataInterface) {
+  const postRef = doc(firestore, "posts", postData.post_name);
   await setDoc(postRef, postData);
 }
-
-
 
 const setDate = () => {
   var nowDate = new Date();
