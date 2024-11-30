c5e25042882aec0418ee230074c7f8239d2861af
light <balathan2004@gmail.com>
2024-11-30 07:53:39 +0530

    cors testing

---
+++ pages/api/test.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest,NextApiResponse } from "next";
+import { cors } from "@/libs/cors";
+
+
+export default (req:NextApiRequest, res:NextApiResponse)=>{
+
+
+    if(cors(req,res)){
+        return;
+    }
+    
+    if(req.method=="POST"){
+        console.log(req.body);
+    }
+
+}
