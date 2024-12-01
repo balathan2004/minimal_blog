03964be32f96854aff4fc400621acb39f7e413c8
light <balathan2004@gmail.com>
2024-12-01 08:58:28 +0530

    cors update

--- components/get_request.ts
+++ components/get_request.ts
@@ -3,15 +3,14 @@ import { Responses } from "./send_data";
 export const GetRequest=async(route:string)=>{
 
 
-  const RequestConfig:RequestInit={
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "Origin":"http://localhost:3000"
-    },
-  }
+ 
 
-  const response = await fetch(route, RequestConfig);
+  const response = await fetch(route,{
+    method: 'GET',
+    headers:{
+      "Origin": "http://localhost:3000"
+    }
+  });
   const responseJson:Responses= await response.json();
   return responseJson;
 
